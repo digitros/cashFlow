d07621ac914e282ab6ddcc4fb885aaf55b4f40e5
Diego Valdez <diego.valdez@improving.com>
2023-01-06 13:34:34 -0600

    Add layout components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
-import Home from "@/components/Home.vue";
+import SplashScreen from "@/components/SplashScreen.vue";
+import { defineAsyncComponent } from "vue";
+
+const Home = defineAsyncComponent(
+  () => import("@/components/HomeComponent.vue")
+);
 </script>
 
 <template>
-  <Home />
+  <Suspense>
+    <template #default>
+      <Home />
+    </template>
+    <template #fallback>
+      <SplashScreen />
+    </template>
+  </Suspense>
 </template>
 
-<style scoped></style>
+<style>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+</style>
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Layout from "./LayoutComponent.vue";
+import Header from "./HeaderComponent.vue";
+import Resume from "./Resume/ResumeComponent.vue";
+import Movements from "./MovementsComponent.vue";
+</script>
+
+<template>
+  <Layout>
+    <template #header>
+      <Header />
+    </template>
+    <template #resume>
+      <div>
+        <Resume />
+      </div>
+    </template>
+    <template #movements>
+      <div>
+        <Movements />
+      </div>
+    </template>
+  </Layout>
+</template>
